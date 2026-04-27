0a6c5b159576fee85f8a8aa1e2932dfd17a35706
Farid1199 <220090483@aston.ac.uk>
2026-04-27 16:25:26 +0100

    Fazz PitchSideHub Email Feature

--- config/settings.py
+++ config/settings.py
@@ -162,7 +162,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'fazzpitchsidehubsupport@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'iqgipuvozinjrbtn')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = f'Fazz PitchSide Hub <{EMAIL_HOST_USER}>'
 
 # --- Production Security (uncomment when deploying) ---
